9a8023e5798358e49b7cd664202abbd3e169cedf
anirudh86611 <2021wa86611@wilp.bits-pilani.ac.in>
2025-06-15 14:30:46 +0530

    Update ServerInfo.java

--- src/main/java/ServerInfo.java
+++ src/main/java/ServerInfo.java
@@ -5,16 +5,7 @@ import java.time.format.DateTimeFormatter;
 public class ServerInfo {
 
     public static void main(String[] args) {
-        LocalDateTime now = LocalDateTime.now();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-        String skadoosh = now.format(formatter);
-
-        String hostname = "Unknown";
-        try {
-            InetAddress ip = InetAddress.getLocalHost();
-            hostname = ip.getHostName();
-        } 
-        System.out.println("Current Time: " + skadoosh);
-        System.out.println("Hostname: " + hostname);
+        System.out.println("2021WA86611 - Anirudh");
+        System.out.println("Devops Lab3");
     }
 }
