3500ba59550b48fbf463cbf585d082419c88e1b8
anirudh86611 <2021wa86611@wilp.bits-pilani.ac.in>
2025-06-15 14:41:06 +0530

    Update ServerInfo.java

--- src/main/java/com/example/ServerInfo.java
+++ src/main/java/com/example/ServerInfo.java
@@ -3,6 +3,6 @@ public class ServerInfo {
 
     public static void main(String[] args) {
         System.out.println("2021WA86611 - Anirudh");
-        System.out.println("Devops Lab3");
+        //System.out.println("Devops Lab3");
     }
 }
