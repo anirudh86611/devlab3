a6dc43fbd68c183955c5c32660cbec4f954f5399
anirudh86611 <2021wa86611@wilp.bits-pilani.ac.in>
2025-07-05 15:30:01 +0530

    original

--- src/main/java/com/example/ServerInfo.java
+++ src/main/java/com/example/ServerInfo.java
@@ -3,7 +3,6 @@ public class ServerInfo {
 
     public static void main(String[] args) {
         System.out.println("2021WA86611 ");
-        System.out.println("2021WA86611 ");
         
     }
 }
